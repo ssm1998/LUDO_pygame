bccb7ea199e06097253cd8fae0d203fafef11b1c
Swastishreya Mishra <swastishreya.mishra@iiitb.org>
2017-10-16 22:48:01 +0530

    Making a screen

---
+++ mainGame.py
@@ -0,0 +1,21 @@
+import pygame, sys
+
+pygame.init()
+
+resolution = (600,400)
+
+white = (255,255,255)
+
+game_display = pygame.display.set_mode(resolution)
+
+pygame.display.set_caption('My first window')
+
+game_display.fill(white)
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
