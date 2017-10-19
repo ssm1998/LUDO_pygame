9f2eb1996bfa4863119a05ac79d132856a7d15d3
Swastishreya Mishra <swastishreya.mishra@iiitb.org>
2017-10-19 12:46:06 +0530

    Player specify function added

--- LUDOfunctions.py
+++ LUDOfunctions.py
@@ -1,5 +1,45 @@
 import random
 
+def playerSpecify():
+    print "There can only be 2, 3 or 4 players..."
+    print "Colors available: Red, Blue, Yellow and Green..." 
+    players = input("Enter number of players: ")
+    if players == 2:
+	color1 = raw_input("Choose color for player 1: ")
+	color2 = raw_input("Choose color for player 2: ")
+	if color2 == color1:
+	    print "Player two can't choose the same color ! Please choose again."
+	    color2 = raw_input("Choose color for player 2: ")
+	    return color1, color2
+    elif players == 3:
+	color1 = raw_input("Choose color for player 1: ")
+	color2 = raw_input("Choose color for player 2: ")
+	if color2 == color1:
+	    print "Two players can't choose the same color ! Please choose again."
+            color2 = raw_input("Choose color for player 2: ")
+        color3 = raw_input("Choose color for player 3: ")
+        if color3 == color1 or color3 == color2:
+    	    print "Two players can't choose the same color ! Please choose again."
+	    color3 = raw_input("Choose color for player 3: ")
+	    return color1, color2, color3
+    elif players == 4:
+	color1 = raw_input("Choose color for player 1: ")
+        color2 = raw_input("Choose color for player 2: ")
+        if color2 == color1:
+            print "Two players can't choose the same color ! Please choose again."
+            color2 = raw_input("Choose color for player 2: ")
+        color3 = raw_input("Choose color for player 3: ")
+        if color3 == color1 or color3 == color2:
+            print "Two players can't choose the same color ! Please choose again."
+            color3 = raw_input("Choose color for player 3: ")
+	color4 = raw_input("Choose color for player 4: ")
+	if color4 == color1 or color4 == color2 or color4 == color3:
+	    print "Two players can't choose the same color ! Please choose again."
+            color4 = raw_input("Choose color for player 4: ")
+            return color1, color2, color3, color4
+    else:
+	playerSpecify()
+
 def startGame():
     '''
     You get to start the game only if you get a six!
@@ -37,3 +77,5 @@ def rollingDie():
     else:
         print "One move!"
         return roll1
+
+print playerSpecify()
